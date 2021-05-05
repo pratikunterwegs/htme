222d878ea95daff3dab6060730b355ffade33d18
Pratik Gupte <pratikgupte16@gmail.com>
2021-05-05 19:48:12 +0200

    add to theme

--- R/fun_theme.R
+++ R/fun_theme.R
@@ -13,13 +13,25 @@ theme_custom <- function(grid = FALSE, border_size = 2.5, ...) {
       panel.border = ggplot2::element_rect(size = border_size),
       legend.position = "none",
       axis.ticks.length = ggplot2::unit(1.5, units = "mm"),
-        axis.title = element_blank(),
-        axis.text.y = element_text(
-          angle = 90,
-          hjust = 0.5
-        ),
-      strip.background = ggplot2::element_rect(colour = NA,
-                                      fill = "lightgrey")
+      axis.title = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_text(
+        face = "bold"
+      ),
+      axis.text.y = ggplot2::element_text(
+        angle = 90,
+        hjust = 0.5,
+        face = "bold"
+      ),
+      strip.background = ggplot2::element_rect(
+        colour = NA,
+        fill = "lightgrey"
+      ),
+      legend.text = ggplot2::element_text(
+        face = "bold"
+      ),
+      legend.title = ggplot2::element_text(
+        face = "bold"
+      )
     )
 
   if (grid) {
@@ -34,4 +46,3 @@ theme_custom <- function(grid = FALSE, border_size = 2.5, ...) {
 
   this_theme
 }
-
