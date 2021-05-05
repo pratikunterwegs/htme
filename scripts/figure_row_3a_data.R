4d80aea6f32600bc7e09fc90fe05f3ca73f1590f
Pratik Gupte <pratikgupte16@gmail.com>
2021-05-05 20:06:05 +0200

    make r script

--- scripts/figure_row_3a_data.R
+++ scripts/figure_row_3a_data.R
@@ -1,10 +1,15 @@
----
-output: html_document
-editor_options: 
-  chunk_output_type: console
----
+#' ---
+#' title: "Data Cleaning"
+#' author: Pratik R Gupte
+#' date: May 2021
+#' output: html_document
+#' editor_options:
+#'   chunk_output_type: console
+#' ---
+#'
 
-```{r}
+#' # Figure 6F: Red Knot Data Cleaning
+#'
 # data processing
 library(data.table)
 library(atlastools)
@@ -14,53 +19,45 @@ library(sf)
 library(ggplot2)
 library(ggspatial)
 library(magick)
-```
 
-```{r}
+#' ## Get data
 # get data
 data <- fread("data/whole_season_tx_0435.csv")
-
 # convert time to seconds from ms
 data[, time := round(TIME / 1000)]
-
 data[, time := as.POSIXct(time, tz = "Amsterdam", origin = "1970-01-01")]
-```
 
-```{r}
+#' ## Filter data
 # filter out before specific time
 data_filter <- atl_filter_covariates(
   data = data,
   filters = "time > '2018-08-19 18:31:00 Amsterdam'"
 )
-
 # select one day after start time
 data_filter <- atl_filter_covariates(
   data = data_filter,
   filters = c("time < (time[1] + (18 * 3600))")
 )
-```
 
-```{r}
+#' ## Basic metrics
 # add speed
 data_filter[, c(
   "speed_in", "speed_out",
   "angle"
 ) := list(
   atl_get_speed(data_filter,
-    x = "X", y = "Y", time = "time",
-    type = "in"
+                x = "X", y = "Y", time = "time",
+                type = "in"
   ),
   atl_get_speed(data_filter,
-    x = "X", y = "Y", time = "time",
-    type = "out"
+                x = "X", y = "Y", time = "time",
+                type = "out"
   ),
   atl_turning_angle(data_filter, x = "X", y = "Y", time = "time")
 )]
-```
 
-## remove sd and speed
-
-```{r}
+#' ## Filter sd and speed
+# filter with atlastools
 data_filter_sd_speed <- atl_filter_covariates(
   data = data_filter,
   filters = c(
@@ -68,24 +65,23 @@ data_filter_sd_speed <- atl_filter_covariates(
     "((speed_in < 20 & speed_out < 20))"
   )
 )
-```
 
-## do median smooth
-
-```{r}
+#' ## Median smooth
+#'
 data_smooth <- copy(data_filter_sd_speed)
 
 atl_median_smooth(
   data = data_smooth,
   x = "X", y = "Y", time = "TIME", moving_window = 11
 )
-```
 
-## plot showing median smooth
+#' ## Save as Rdata
+save(data_filter, data_smooth, file = "data/data_figure_6F.Rdata")
 
-```{r}
+#' ## Make figure 6F
+#'
 # set limits
-xlim <- c(650600, 650950)
+xlim <- c(650600, 650850)
 ylim <- c(5901815, 5902000)
 
 legend_label <- c(
@@ -96,14 +92,13 @@ legend_label <- c(
 )
 
 # explore raw
-figure_3a_prelim <-
-  ggplot() +
+fig_6F =
+ggplot() +
   geom_path(
     data = data_filter[time < time[1] + 12*3600 &
                          time > time[1] + 4*3600,],
     aes(X, Y,
-      col = "raw",
-      shape = "raw"
+        col = "raw"
     ),
     size = 0.3
   ) +
@@ -111,8 +106,7 @@ figure_3a_prelim <-
     data = data_smooth[time < time[1] + 12*3600 &
                          time > time[1] + 4*3600,],
     aes(X, Y,
-      col = "smooth",
-      shape = "smooth"
+        col = "smooth"
     )
     # size = 0.3
   ) +
@@ -139,99 +133,43 @@ figure_3a_prelim <-
     labels = legend_label,
     breaks = names(legend_label)
   ) +
-  scale_y_continuous(breaks = 
-                       c(53.245)
-                     ) +
+  scale_y_continuous(
+    breaks = c(53.245)
+  ) +
   coord_sf(
     xlim = xlim, ylim = ylim,
     crs = 32631
-  ) +
-  theme_test(base_family = "Arial") +
-  theme(
-    legend.position = c(0.6, 0.8),
-    legend.text = element_text(colour = "grey50"),
-    legend.background = element_rect(
-      fill = "white",
-      color = NA
-    ),
-    panel.background = element_rect(
-      colour = NA
-    ),
-    axis.text.y = element_text(
-      angle = 90,
-      hjust = 0.5
-    ),
-    axis.title = element_blank()
   ) +
   annotation_scale(
     bar_cols = c("grey20"),
     height = unit(1, units = "mm"),
     style = "ticks",
     line_width = 1,
+    text_face = "bold",
     line_col = "grey20",
     text_col = "grey20",
     width_hint = 0.175
-  )
-```
-
-## add inset map
-
-```{r}
-# get data
-nl = st_as_sf(rnaturalearthdata::countries50)
-nl = nl[nl$continent == "Europe", ]
-
-# make map
-inset =
-  ggplot(nl)+
-    geom_sf(fill = "khaki", size = 0.2)+
-    geom_point(
-      x = 5, y = 53,
-      shape = 21,
-      size = 3,
-      fill = "indianred1"
-    )+
-    coord_sf(
-      xlim = c(-5, 10.5),
-      ylim = c(43.5, 60)
-    )+
-    theme_void()+
-    theme(
-      plot.background = element_rect(
-        fill = "aliceblue",
-        colour = "black",
-        size = 0.2
-      ),
-      panel.ontop = F
+  )+
+  htme::theme_custom(base_size = 7, base_family = "Arial")+
+  theme(
+    legend.position = c(0.8, 0.8),
+    legend.background = element_rect(
+      fill = "white",
+      color = NA
     )
-```
-
-```{r}
-figure_3a_prelim = 
-  figure_3a_prelim +
-  annotation_custom(
-    ggplotGrob(inset),
-    xmin = xlim[2] - 60,
-    xmax = xlim[2],
-    ymin = ylim[2] - 100,
-    ymax = ylim[2]
   )
-```
 
-
-```{r}
+# save image as prelim
 ggsave(
-  figure_3a_prelim,
-  filename = "figures/figure_row_3_a_preprocessing_prelim.png",
-  height = 2.75, width = 5
+  fig_6F,
+  filename = "figures/fig_6F.png",
+  height = 3, width = 4
 )
-```
 
-## add red knot
-
-```{r}
+#' ## Add red knot
+#'
 # get figure
-im <- image_read("figures/figure_row_3_a_preprocessing_prelim.png")
+im <- image_read("figures/fig_6F.png")
 
 # get red knot
 knot <- image_read("figures/knots/knot_poster_picture.png")
@@ -243,13 +181,11 @@ im <-
     image_scale(knot, "60%x"),
     operator = "Atop",
     gravity = "SouthWest",
-    offset = "-10-150"
+    offset = "+100+120"
   )
 
 # write
 image_write(
   im,
-  path = "figures/figure_row_3_a_preprocessing.png"
+  path = "figures/fig_6F.png"
 )
-```
-
