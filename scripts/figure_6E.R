565429eb1be051f2b14e70e840a428d66933072f
Pratik Gupte <pratikgupte16@gmail.com>
2021-05-12 10:12:12 +0200

    edit fig6E per chris

--- scripts/figure_6E.R
+++ scripts/figure_6E.R
@@ -13,10 +13,12 @@
 #+
 # data processing
 library(data.table)
+library(sf)
 
 # plotting
 library(ggplot2)
 library(ggspatial)
+library(OpenStreetMap)
 library(htme)
 
 #' ## Prepare bat data
@@ -31,6 +33,36 @@ data <- data[SD < 20, ]
 data[, c("xround", "yround") := list(round(X, -2), round(Y, -2))]
 data_summary <- data[, .N, by = c("xround", "yround")]
 
+#' ## Convert coordinates
+data_summary = st_as_sf(data_summary, coords = c("xround", "yround"),
+                        crs= 2039)
+data_summary = cbind(
+  st_drop_geometry(data_summary),
+  round(
+    st_coordinates(
+      st_transform(data_summary, crs = 4326)
+  ), 3)
+)
+
+# sum again
+setDT(data_summary)
+data_summary = data_summary[, .(N = mean(N)), by = c("X", "Y")]
+
+#' ## Get basemap
+basemap = openmap(
+  upperLeft = c(max(data_summary$Y), min(data_summary$X)),
+  lowerRight = c(min(data_summary$Y), max(data_summary$X)),
+  type = "bing"
+)
+
+# project basemap
+basemap = openproj(basemap, projection = st_crs(4326)$proj4string)
+
+# convert all to transparency
+# set transparency level
+alphalevel = 0.3
+basemap$tiles[[1]]$colorData = alpha(basemap$tiles[[1]]$colorData, alphalevel)
+
 #' ## Read trees and roosts
 #'
 #+
@@ -38,12 +70,23 @@ data_summary <- data[, .N, by = c("xround", "yround")]
 trees <- fread("data/trees_update_clusters_Aug2020.csv")
 roosts <- fread("data/Roosts.csv")
 
+# transform to wgs84
+trees = st_as_sf(trees, coords = c("X", "Y"), crs = 2039) %>%
+  st_transform(4326)
+trees = cbind(st_drop_geometry(trees), st_coordinates(trees))
+roosts = st_as_sf(roosts, coords = c("X", "Y"), crs = 2039) %>%
+  st_transform(4326)
+roosts = cbind(st_drop_geometry(roosts), st_coordinates(roosts))
+# remake data summary
+# data_summary <- data[, .N, by = c("xround", "yround")]
+
 #' ## Save as Rdata
 #'
 #+
 # save elements for plot as rdata
 save(data_summary, trees, roosts,
-     file = "data/data_fig_6E.Rdata")
+     file = "data/data_fig_6E.Rdata",
+     basemap)
 
 # load saved Rdata
 load("data/data_fig_6E.Rdata")
@@ -52,28 +95,29 @@ load("data/data_fig_6E.Rdata")
 #'
 #+ save as object
 fig_6E <-
-  ggplot(data_summary) +
+autoplot(basemap)+
+# ggplot()+
   geom_point(
     data = trees,
     aes(X, Y,
-      col = "trees",
-      shape = "trees"
+        col = "trees",
+        shape = "trees"
     ),
-    size = 0.05
-  ) +
-  geom_tile(aes(xround, yround,
-    fill = N
-  ),
-  alpha = 0.8
-  ) +
+    stroke = 0.3
+  )+
+  geom_tile(
+    data = data_summary,
+    aes(X, Y,
+        fill = N
+    )
+  )+
   geom_point(
     data = roosts,
     aes(X, Y,
       col = "Roost",
       shape = "Roost"
     ),
-    size = 2,
-    stroke = 0.6
+    size = 2
   ) +
   scale_colour_manual(
     values = c(
@@ -85,20 +129,26 @@ fig_6E <-
   scale_shape_manual(
     values = c(
       "Roost" = 5,
-      "trees" = 16
+      "trees" = 2
     ),
     labels = c("Roost", "Tree")
   ) +
-  scale_fill_viridis_c(
-    option = "F",
+  scale_fill_gradientn(
+    colours = pals::jet(25),
+    # option = "F",
     breaks = c(10, 100, 1000),
     labels = c("100", "1,000", "10,000"),
-    trans = "log10",
-    direction = -1
+    trans = "log10"
+    # direction = -1
   ) +
   guides(shape = guide_legend(override.aes = list(size = 2, 1))) +
-  scale_y_continuous(breaks = c(33.05, 33.10)) +
-  scale_x_continuous(breaks = c(35.6)) +
+  scale_y_continuous(
+    breaks = seq(33.05, 33.12, 0.03),
+    labels = function(x) sprintf("%.3f°N", x)
+  ) +
+  scale_x_continuous(
+    breaks = seq(35.57, 35.63, 0.02),
+    labels = function(x) sprintf("%.3f°E", x)) +
   annotation_scale(
     bar_cols = c("grey20"),
     height = unit(1, units = "mm"),
@@ -108,26 +158,37 @@ fig_6E <-
     line_col = "grey20",
     text_col = "grey20",
     width_hint = 0.175,
-    location = "br"
+    location = "tl"
   ) +
   ggplot2::coord_sf(
-    crs = 2039,
-    xlim = range(data$X),
-    ylim = range(data$Y),
+    crs = 4326,
+    xlim = range(data_summary$X),
+    ylim = range(data_summary$Y),
     expand = F
   ) +
   labs(
     fill = "Fixes",
     shape = NULL,
     colour = NULL
-  )+
+  ) +
   htme::theme_custom(base_size = 7, base_family = "Arial")+
   theme(
-    legend.position = c(0.85, 0.75),
+    legend.position = c(0.85, 0.8),
+    legend.background = element_blank(),
+    legend.key = element_rect(
+      fill = NA
+    ),
+    legend.text = element_text(
+      size = 10.5
+    ),
+    legend.title = element_text(
+      size = 10.5
+    ),
     legend.text.align = 1,
     legend.key.width = unit(2, units = "mm"),
     legend.key.height = unit(3, units = "mm"),
     axis.text = element_text(
+      size = 10,
       face = "bold"
     )
   )
@@ -143,7 +204,7 @@ ggsave(
   fig_6E,
   dpi = 350,
   filename = "figures/fig_6E.png",
-  height = 4, width = 3
+  height = 6, width = 4.5
 )
 
 #' Show Figure 6E
